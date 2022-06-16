7a7a8311a2923744f8f19a01f7898c704d5da621
chanjin5212 <chanjin5212@gmail.com>
2022-06-17 03:05:12 +0900

    6/17 update

---
+++ src/main/java/com/spring/cj/springstudy/config/JpgConfig.java
@@ -0,0 +1,9 @@
+package com.spring.cj.springstudy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpgConfig {
+}
