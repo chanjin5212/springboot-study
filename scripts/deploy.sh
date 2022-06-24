d5b603f58af901055cafad64b8997f9884299a5e
chanjin5212 <chanjin5212@gmail.com>
2022-06-24 22:22:39 +0900

    ch10. 무중단 서비스

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/step2
-PROJECT_NAME=springboot-webservice
+PROJECT_NAME=springstudy
 
 echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
-CURRENT_PID=$(ps -ef | grep $PROJECT_NAME | grep jar | awk '{print $2}')
+CURRENT_PID=$(ps -ef | grep ${PROJECT_NAME} | grep jar | awk '{print $2}')
 
 echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
